f2178654751044d2219122eecf2373b01f8bc5c6
Admin <Admin@192.168.1.17>
2024-08-02 12:27:25 +0530

    First Commit

---
+++ src/main/java/org/Project/JPA/App.java
@@ -0,0 +1,13 @@
+package org.Project.JPA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
